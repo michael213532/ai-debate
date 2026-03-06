9f622a2004223480283a1c20429ea5ceb02df8a1
User <user@example.com>
2026-03-06 18:02:36 +0400

    Expert bee 36px everywhere

--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -561,7 +561,7 @@ function addAiDiscussionMessage(modelName, provider, content, personalityId) {
     let emojiHtml = '';
     if (personalityId) {
         if (personalityId === 'expert') {
-            emojiHtml = '<img src="/bee-expert.png" alt="" style="width: 24px; height: 24px; vertical-align: middle; margin-right: 6px;">';
+            emojiHtml = '<img src="/bee-expert.png" alt="" style="width: 36px; height: 36px; vertical-align: middle; margin-right: 6px;">';
         } else {
             const personality = (window.allPersonalities || []).find(p => p.id === personalityId);
             if (personality && personality.emoji) {
